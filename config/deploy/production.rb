f77b6b41ff554317a5e014986f010c5c19c6af39
Chad Jewsbury <cjewsbury@groupon.com>
2014-12-14 20:05:04 -0800

    Updates deploy scripts

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,9 +3,9 @@
 ############################################
 
 set :stage, :production
-set :stage_url, "http://www.example.com"
-server "XXX.XXX.XX.XXX", user: "SSHUSER", roles: %w{web app db}
-set :deploy_to, "/deploy/to/path"
+set :stage_url, "104.236.188.37"
+server "104.236.188.37", user: "deployer", roles: %w{web app db}, port: 61234
+set :deploy_to, "/var/www/apps/katekorroch"
 
 ############################################
 # Setup Git
